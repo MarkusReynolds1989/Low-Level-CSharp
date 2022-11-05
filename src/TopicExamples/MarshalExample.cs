8df100369d738f08e66e744e37b593dda3565645
Markus Reynolds <markusreynolds1989@gmail.com>
2022-11-05 16:01:14 -0500

    UnsafeMarshal example and test

--- src/TopicExamples/MarshalExample.cs
+++ src/TopicExamples/MarshalExample.cs
@@ -1,11 +1,9 @@
-﻿using System.Runtime.InteropServices;
-
-namespace TopicExamples;
+﻿namespace TopicExamples;
 
 public static class MarshalExample
 {
     // This has to be unsafe because we are using a pointer to unmanaged memory.
-    private static unsafe void ForeignUnmanagedFunction(byte* charList, uint length)
+    public static unsafe void ForeignUnmanagedFunction(byte* charList, uint length)
     {
         // Notice here that when you want to dereference the field of a pointer you have to use the -> arrow operator.
         // If you know C++ or C this will be familiar for you.
@@ -30,34 +28,5 @@ public static class MarshalExample
             first += 1;
             last -= 1;
         }
-    }
-
-    public static void LocalMain()
-    {
-        // In some circumstances we may need to pass unmanaged data to an unmanaged context.
-        // In that case we would use Marshalling to handle for that.
-
-        // A managed string.
-        var palindrome = "racecar";
-        var length = palindrome.Length - 1;
-
-        // Now we have an unmanaged pointer.
-        var pointer = Marshal.StringToHGlobalAuto(palindrome);
-
-        // Pass a string that hasn't been assigned yet.
-        var otherPointer = Marshal.StringToHGlobalAuto("Other");
-
-        // Move the pointer to some unmanaged context.
-        unsafe
-        {
-            // We are passing a byte array to the FFI, this might look like char* in other languages like C and C++.
-            ForeignUnmanagedFunction((byte*) pointer, (uint) length);
-            // Get a string back from the pointer.
-            var managedName = Marshal.PtrToStringAuto(pointer);
-            Console.WriteLine(managedName);
-        }
-
-        // Remember to free the pointer.
-        Marshal.FreeHGlobal(pointer);
     }
 }
---
+++ src/TopicTests/UnsafeMarshalTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TopicTests;
+
+public class UnsafeMarshalTests
+{
+    [Fact]
+    public void Basics()
+    {
+        // Marshaling is when we want to control the memory of our data
+        // between unmanaged and managed contexts.
+        // Managed being GC code and unmanaged being non-GC code.
+
+        // This fits in nicely with how unsafe works, it allows us to 
+        // work with our memory in an environment that not only may not be using the GC,
+        // but also has many safety checks turned off, and allows us to directly
+        // touch memory addresses.
+
+        // Unsafe will NOT make magically make slow code fast, it is more
+        // for use in circumstances where we don't have any other choice
+        // but to use raw pointers and we need to avoid using the GC and 
+        // bounds checking for whatever reason.
+    }
+
+    [Fact]
+    public void PalindromeCheck()
+    {
+        // A managed string.
+        const string palindrome = "racecar";
+        var length = palindrome.Length - 1;
+
+        // Now we have an unmanaged pointer.
+        var pointer = Marshal.StringToHGlobalAuto(palindrome);
+
+        // Pass a string that hasn't been assigned yet.
+        var otherPointer = Marshal.StringToHGlobalAuto("Other");
+
+        // Move the pointer to some unmanaged context.
+        unsafe
+        {
+            // We are passing a byte array to the FFI, this might look like char* in other languages like C and C++.
+            MarshalExample.ForeignUnmanagedFunction((byte*) pointer, (uint) length);
+            // Get a string back from the pointer.
+            var managedName = Marshal.PtrToStringAuto(pointer);
+
+            Assert.Equal(managedName, palindrome);
+        }
+    }
+}
