f7d4d809c381189e168a003557e2e665ef21f982
Markus Reynolds <markusreynolds1989@gmail.com>
2022-11-05 15:30:52 -0500

    Struct tests

--- src/TopicExamples/StructExample.cs
+++ src/TopicExamples/StructExample.cs
@@ -1,5 +1,7 @@
 ﻿namespace TopicExamples;
 
+// If you see the IL code for this, it will show you that it's a value type 
+// as opposed to an object(reference) type.
 struct Example
 {
     private int   x;
@@ -11,7 +13,7 @@ public static class StructExample
     // Defining it as a "ref struct" has guarantees that it won't move to the heap.
     // This is our way of avoiding the implementation details problem where 
     // we can't be sure if an item is on the heap or stack.
-    private ref struct Person
+    public ref struct Person
     {
         private ReadOnlyMemory<char> Name { get; }
         public  int                  Age  { set; get; }
@@ -23,7 +25,7 @@ public static class StructExample
         }
     }
 
-    private class PersonC
+    public class PersonC
     {
         public int Age { get; set; }
 
@@ -33,14 +35,14 @@ public static class StructExample
         }
     }
 
-    private static void AddYearAge(PersonC person)
+    public static void AddYearAge(PersonC person)
     {
         // Will mutate because the person class is a reference type.
         // Which means it will be passed by reference.
         person.Age += 1;
     }
 
-    private static void AddYearAge(Person person)
+    public static void AddYearAge(Person person)
     {
         // Won't mutate the person because it was passed by value (value type). 
         // This is a copy, think of it as a brand new allocation of person localized in this stack frame.
@@ -48,31 +50,10 @@ public static class StructExample
     }
 
     // How can we mutate the struct? Pass by reference.
-    private static void AddYearAge(ref Person person)
+    public static void AddYearAge(ref Person person)
     {
         // Although this struct is pass by value, we are passing it by reference so it will be used
         // as if it were a reference type.
         person.Age += 1;
-    }
-
-    public static void LocalMain()
-    {
-        // This person object is a value type and is stack allocated. 
-        var tom = new Person("tom".AsMemory(), 32);
-        Console.WriteLine(tom.Age);
-        AddYearAge(tom);
-        Console.WriteLine(tom.Age);
-
-        var tim = new PersonC(32);
-        Console.WriteLine(tim.Age);
-        AddYearAge(tim);
-        Console.WriteLine(tim.Age);
-
-        // When we pass the struct by reference we an mutate it.
-        // It is still on the stack, but we are not passing a copy of it, but a reference instead.
-        // This allows mutation.
-        AddYearAge(ref tom);
-
-        Console.WriteLine(tom.Age);
     }
 }
---
+++ src/TopicTests/StructExamples.cs
@@ -0,0 +1,37 @@
+﻿namespace TopicTests;
+
+public class StructExamples
+{
+    [Fact]
+    public void ImmutabilityTest()
+    {
+        // This person object is a value type and is stack allocated. 
+        var tom = new StructExample.Person("tom".AsMemory(), 32);
+        Assert.Equal(tom.Age, 32);
+        // Cannot be mutated because it's passed by value.
+        StructExample.AddYearAge(tom);
+        Assert.Equal(tom.Age, 32);
+    }
+
+    [Fact]
+    public void RefTest()
+    {
+        var tom = new StructExample.Person("tom".AsMemory(), 32);
+        Assert.Equal(tom.Age, 32);
+        // Passing this value type by reference makes it to where we can mutate it.
+        // It also doesn't get copied in this case.
+        // Which saves us memory.
+        StructExample.AddYearAge(ref tom);
+        Assert.Equal(tom.Age, 33);
+    }
+
+    [Fact]
+    public void ClassTest()
+    {
+        // Classes are pass by reference by default, so we can mutate them
+        // when we pass them in.
+        var tim = new StructExample.PersonC(32);
+        StructExample.AddYearAge(tim);
+        Assert.Equal(tim.Age, 33);
+    }
+}
